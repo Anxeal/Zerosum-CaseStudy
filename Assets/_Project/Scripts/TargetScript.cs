3cb34eb6c4c79654e3f0cca8966ee92807e9e17e
Unknown <anxealous@gmail.com>
2019-12-10 21:54:42 +0300

    Create simple target block
    Added rigidbody to the static target from earlier

---
+++ Assets/_Project/Scripts/TargetScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
