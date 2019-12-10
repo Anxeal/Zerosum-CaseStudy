090666ddc48e9ad2013a8261e28266e03721fa81
Unknown <anxealous@gmail.com>
2019-12-10 23:08:00 +0300

    Make GameManager a singleton

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance { get; private set; }
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Instance != null) Destroy(gameObject);
+        Instance = this;
     }
 }
