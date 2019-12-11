805a626a6966b11ebdc382ef5283c680d3b17056
Unknown <anxealous@gmail.com>
2019-12-11 05:08:44 +0300

    Add explosion effect

---
+++ Assets/ExplosionScript.cs
@@ -0,0 +1,16 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    void Start()
+    {
+        DOTween.Sequence()
+            .SetEase(Ease.OutCubic)
+            .Append(transform.DOScale(0, .5f).From())
+            .Append(GetComponent<MeshRenderer>().material.DOColor(new Color(1,1,1,0), "_ExplosionColor", .3f))
+            .AppendCallback(() => Destroy(gameObject));
+    }
+}
